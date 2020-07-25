b12c49aeca6ab5d3d1004b8c091174442d218027
Jacob Ososke <jmososke@gmail.com>
2020-07-24 22:33:01 -0700

    waypoint update fixes

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -57,7 +57,7 @@ class WaypointUpdater(object):
     def get_closest_waypoint_idx(self):
         x = self.pose.pose.position.x
         y = self.pose.pose.position.y
-        closest_idx = self.waypoint_tree.queue([x,y],1)[1]
+        closest_idx = self.waypoint_tree.query([x,y],1)[1]
 
         # check if closest is ahead or behind vehicle
         closest_coord = self.waypoints_2d[closest_idx]
@@ -78,7 +78,7 @@ class WaypointUpdater(object):
     def publish_waypoints(self, closest_idx):
         lane = Lane()
         lane.header = self.base_waypoints.header
-        lane.waypoints = self.base_waypoints.waypoint[closest_idx:closest_idx + LOOKAHEAD_WPS]
+        lane.waypoints = self.base_waypoints.waypoints[closest_idx:closest_idx + LOOKAHEAD_WPS]
         self.final_waypoints_pub.publish(lane)
 
     def pose_cb(self, msg):
